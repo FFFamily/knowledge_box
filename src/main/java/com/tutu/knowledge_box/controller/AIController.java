89d152e3f954a547aaadfb89e719e3fba73d2e0e
FFFamily <tjj747487547@sina.com>
2023-04-04 16:05:21 +0800

    测试

--- src/main/java/com/tutu/knowledge_box/controller/AIController.java
+++ src/main/java/com/tutu/knowledge_box/controller/AIController.java
@@ -1,7 +1,24 @@
 package com.tutu.knowledge_box.controller;
 
+import com.tutu.knowledge_box.entity.BaseResponse;
+import com.tutu.knowledge_box.service.AIService;
+import com.tutu.knowledge_box.vo.QuestionVo;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AIController {
+//    @Resource
+//    private AIService aiService;
+
+    /**
+     * 询问问题
+     */
+    @PostMapping("/ask")
+    public BaseResponse askQuestion(QuestionVo questionVo){
+//        String result = aiService.askQuestionFromAI(questionVo.getQuestion());
+//        return BaseResponse.success(result);
+        return BaseResponse.SUCCESS();
+    }
 }
---
+++ src/main/java/com/tutu/knowledge_box/vo/QuestionVo.java
@@ -0,0 +1,11 @@
+package com.tutu.knowledge_box.vo;
+
+import lombok.Data;
+
+@Data
+public class QuestionVo {
+    /**
+     * 问题
+     */
+    private String question;
+}
